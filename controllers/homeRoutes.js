4a94089e1dc1c7cc379524250bcae90463ac958e
Michael Salisbury <m.slzbry@gmail.com>
2023-07-18 20:30:42 -0700

    fix error

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -17,7 +17,7 @@ router.get('/', async (req, res) => {
     const runs = runData.map(run => run.get({ plain: true }))
 
     // Pass serialized data and session flag into template
-    res.render('runs', {
+    res.render('homepage', {
       runs,
       logged_in: req.session.logged_in
     })
